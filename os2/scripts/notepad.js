42b3ffef6f359e37bd4e9295a9b51c64aa93f269
Shania <shania@Shanias-MacBook-Pro.local>
2023-04-02 01:11:14 +1100

    created first app - enabled app to open + close

---
+++ os2/scripts/notepad.js
@@ -0,0 +1,49 @@
+let isAppOpen = false;
+
+notepadIcon.addEventListener("dblclick", () => {
+  if (isAppOpen) {
+    //exit function/event if app open
+    return;
+  }
+  isAppOpen = true;
+
+  createElement("div", "window", desktop);
+
+  const windowDiv = document.querySelector(".window");
+
+  const notepadContent = `
+  <div class="window__menu">
+    <div class="window__menu__one">
+      <div class="window__menu__one--title">
+        <img src="./assets/notepad.png" alt="">
+        <p>Untitled - Notepad</p>
+      </div>
+
+      <div class="window__menu__one--buttons">
+        <p class="window-button" id="minimise">—</p>
+        <p class="window-button" id="miaxmise">ロ</p>
+        <p class="window-button" id="close">&#x2715</p>
+      </div>
+    </div>
+    <div class="window__menu__two">
+      <p>File</p>
+      <p>Edit</p>
+      <p>Format</p>
+      <p>View</p>
+      <p>Help</p>
+    </div>
+  </div>
+  <div class="window__main">
+    <textarea id="text-area"></textarea>
+  </div>
+`;
+
+  windowDiv.innerHTML = notepadContent;
+
+  const closeBtn = document.getElementById("close");
+
+  closeBtn.addEventListener("click", () => {
+    windowDiv.remove();
+    isAppOpen = false;
+  });
+});
---
+++ os2/scripts/scripts.js
@@ -0,0 +1,32 @@
+console.log("is linked");
+
+// // bin shortcut on desktop
+// const recycleBin = document.getElementById("bin");
+// console.dir(recycleBin);
+
+// // right side of screen
+// const screenRight = document.querySelector(".wallpaper--right");
+// console.dir(screenRight);
+
+// function to create window
+const createElement = (elementType, classList, parentNode) => {
+  const newElement = document.createElement(elementType);
+  newElement.classList.add(classList);
+  parentNode.appendChild(newElement);
+};
+
+//function to add html to div
+
+// const appendHtml = () => {
+//   const window = document.querySelector("window");
+//   window.innerHTML += "<p>This is some new HTML code</p>";
+// };
+
+// recycleBin.addEventListener("click", () => {
+//   console.log("bin is clicked");
+//   //   alert("bin is clicked");
+//   createElement("div", "window", document.querySelector(".wallpaper--right"));
+// });
+
+const notepadIcon = document.querySelector(".notepad-icon");
+const desktop = document.querySelector(".desktop__main");
