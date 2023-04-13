f311a0b42cf3998450735312ce217d6dd4539c3a
sswolter <121854262+sswolter@users.noreply.github.com>
2023-04-13 22:45:17 +1000

    Revert "Develop"

--- scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-const desktop = document.querySelector(".desktop__main");
-
-const createElement = (elementType, classList, id, parentNode) => {
-  const newElement = document.createElement(elementType);
-  newElement.classList.add(classList);
-  newElement.id = id;
-  parentNode.appendChild(newElement);
-};
